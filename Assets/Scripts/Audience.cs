6e1cdcd51f8a93a47a7f84ca69601837d0c1c2e7
Nuker0s <nuker0s2@gmail.com>
2024-01-03 18:05:18 +0100

    A

--- Assets/Scripts/Audience.cs
+++ Assets/Scripts/Audience.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Audience : MonoBehaviour
 {
+    public List<GameObject> throwables = new List<GameObject>();
+    public float cooldown = 1;
+    public int throwers = 1;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,22 @@ public class Audience : MonoBehaviour
     void Update()
     {
         
+    }
+    public IEnumerator audience() 
+    {
+        while (true) 
+        {
+            for (int i = 0; i < throwers; i++)
+            {
+                yeet(transform.GetChild(Random.Range(0, transform.childCount)).position);
+            }
+            
+            yield return new WaitForSeconds(cooldown);
+        }
+        
+    }
+    public void yeet(Vector3 from) 
+    {
+
     }
 }
