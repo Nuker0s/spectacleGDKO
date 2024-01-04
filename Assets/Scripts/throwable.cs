4a98fe1911b8e50d0b9d6eae4bace1aa1bf16039
Nuker0s <nuker0s2@gmail.com>
2024-01-04 23:07:35 +0100

    a

--- Assets/Scripts/throwable.cs
+++ Assets/Scripts/throwable.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class throwable : MonoBehaviour
 {
-    public Vector3 target;
+    public Transform target;
     public float speed;
+    public Vector3 startpos;
+    public AnimationCurve flightpath;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startpos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, target, speed);
+        transform.position = Vector3.Lerp(transform.position, target.position, speed);
+        if (Vector3.Distance(transform.position,target.position) > 0.2f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
